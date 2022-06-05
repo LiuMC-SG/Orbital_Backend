a1ae908b01bc364a5fd65b0cb61ebfd9b22a6793
Liu Muchen <liumuchen1@gmail.com>
2022-06-06 00:50:31 +0800

    (Update): Allow all CORS

--- HelpUS/HelpUS/settings.py
+++ HelpUS/HelpUS/settings.py
@@ -62,12 +62,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'HelpUS.urls'
 
-CORS_ALLOWED_ORIGINS = [
-    "https://helpus-349601.web.app",
-    "https://helpus-349601.firebaseapp.com",
-    "http://localhost",
-    "http://127.0.0.1"
-]
+CORS_ORIGIN_ALLOW_ALL = True
 
 TEMPLATES = [
     {
